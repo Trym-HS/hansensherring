ce513cacc1fcf4ead6b46288fc42594c0ae83c8d
Trym-HS <trska2186@uib.no>
2026-03-21 11:09:01 +0100

    first commit

---
+++ data-raw/herring.R
@@ -0,0 +1,6 @@
+## code to prepare `herring` dataset goes here
+
+usethis::use_data(herring, overwrite = TRUE)
+
+# All data for the project including code for processing, cleaning and importing is used here
+
